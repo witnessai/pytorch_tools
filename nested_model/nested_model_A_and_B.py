af5285cb0150b3ef1ad11323db79003220d7d0bd
witnessai <kario8023@gmail.com>
2019-12-02 21:17:22 +0800

    add nested model

---
+++ nested_model/nested_model_A_and_B.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class B(torch.nn.Module):
+    def __init__(self):
+        super(B, self).__init__()
+        self.fc = nn.Linear(100, 100)
+    def forward(self, input):
+        x = self.fc(input)
+        return x
+modeltwo = B()
+
+class A(torch.nn.Module):
+    def __init__(self):
+        super(A, self).__init__()
+        self.fc1 = nn.Linear(100, 100)
+        self.fc2 = modeltwo
+    def forward(self, input):
+        x = self.fc1(input)
+        x = self.fc2(x)
+        return x
+
+
+
+
+modelone = A()
+
+input = torch.rand(10, 100)
+output = modelone(input)
+print(output)
